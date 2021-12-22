19731cb8f60c48cd6e1e2377f54d1c9ec41a0873
Benjamin Fadina <benjaminsqlserver@gmail.com>
2021-12-22 07:51:28 +0100

    first commit

---
+++ server/Pages/AddCustomer.razor.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Radzen;
+using Radzen.Blazor;
+
+namespace IyaElepoApp.Pages
+{
+    public partial class AddCustomerComponent
+    {
+        private async System.Threading.Tasks.Task AddNewCustomerAsync()
+        {
+            try
+            {
+               IyaElepoApp.Models.ConData.Customer conDataCreateCustomerResult = await ConData.CreateCustomer(customer);
+
+                if(conDataCreateCustomerResult!=null)
+                {
+                    if(conDataCreateCustomerResult.CustomerID > 0)//successful customer creation
+                    {
+                       // string[] roles = { "Customer" };
+                        var newUser = new Models.ApplicationUser { UserName = customer.Email, Email = customer.Email, Password = "Password_1", ConfirmPassword = "Password_1" };
+                        newUser.RoleNames = new List<string>();
+                        newUser.RoleNames.Append("Customer");
+                        var createUserResult = await Security.CreateUser(newUser);
+
+                        if(!string.IsNullOrEmpty(createUserResult.Id))
+                        {
+                            string[] customerNames = customer.CustomerName.Split(" ");
+                            var existingUser = await ConData.GetAspNetUserById(createUserResult.Id);
+                            if(existingUser!=null)
+                            {
+                                if(!string.IsNullOrEmpty(existingUser.Id))
+                                {
+                                    existingUser.GenderID = GenderID;
+                                    if(customerNames.Length > 0 && customerNames.Length < 2)
+                                    {
+                                        existingUser.FirstName = customerNames[0];
+                                    }
+                                   else if (customerNames.Length > 0 && customerNames.Length < 3)
+                                    {
+                                        existingUser.FirstName = customerNames[0];
+                                        existingUser.Surname = customerNames[1];
+                                    }
+                                    else if (customerNames.Length > 2)
+                                    {
+                                        existingUser.FirstName = customerNames[0];
+                                        existingUser.MiddleName = customerNames[1];
+                                        existingUser.Surname = customerNames[2];
+                                    }
+
+                                    existingUser.CustomerID = conDataCreateCustomerResult.CustomerID;
+                                    
+                                    var updateResult = await ConData.UpdateAspNetUser(createUserResult.Id, existingUser);
+                                }
+                                else
+                                {
+                                    NotificationService.Notify(new NotificationMessage() { Severity = NotificationSeverity.Error, Summary = $"Error", Detail = "Customer Creation Error", Duration = 7000 });
+                                }
+                            }
+                            NotificationService.Notify(new NotificationMessage() { Severity = NotificationSeverity.Success, Summary = $"Successful Customer Creation ", Detail = "Successful Customer Creation", Duration = 7000 });
+                            // DialogService.Close(customer);
+                            UriHelper.NavigateTo("customers", true);
+                        }
+                        else
+            {
+                NotificationService.Notify(new NotificationMessage() { Severity = NotificationSeverity.Error, Summary = $"Error", Detail = "Customer Creation Error", Duration = 7000 });
+            }
+
+        }
+                    else
+                    {
+                        NotificationService.Notify(new NotificationMessage() { Severity = NotificationSeverity.Error, Summary = $"Error", Detail = "Customer Creation Error", Duration = 7000 });
+                    }
+                }
+               
+            }
+            catch (System.Exception conDataCreateCustomerException)
+            {
+                NotificationService.Notify(new NotificationMessage() { Severity = NotificationSeverity.Error, Summary = $"Error", Detail =conDataCreateCustomerException.Message,Duration=7000 });
+            }
+        }
+    }
+}
---
+++ server/Services/ConDataService.Custom.cs
@@ -0,0 +1,23 @@
+﻿using Radzen;
+using System;
+using System.Web;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Data;
+using System.Text.Encodings.Web;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Components;
+using IyaElepoApp.Data;
+using IyaElepoApp.Models.ConData;
+
+namespace IyaElepoApp
+{
+    public partial class ConDataService
+    {
+        
+    }
+}
